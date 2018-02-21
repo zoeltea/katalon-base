67e8f72d21e62c215c62262ade3ebd7d8da196dd
zul.yatman <zul.yatman@aprisma.co.id>
2018-02-21 15:26:36 +0700

    test

--- katalon/src/scripts/katalon-execute.sh
+++ katalon/src/scripts/katalon-execute.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
-
 set -xe
-
 echo "Starting Katalon Studio"
-
 current_dir=$(pwd)
-
 # create tmp directory
 tmp_dir=$KATALON_KATALON_ROOT_DIR/tmp
 mkdir -p $tmp_dir
@@ -15,6 +11,7 @@ chmod -R 777 $tmp_dir
 project_dir=$KATALON_KATALON_ROOT_DIR/project
 mkdir -p $project_dir
 cp -r $KATALON_KATALON_ROOT_DIR/source/ $project_dir
+
 # create .classpath if not exist
 touch $project_dir/.classpath || exit
 chmod -R 777 $project_dir
@@ -27,9 +24,7 @@ chmod -R 777 $report_dir
 # build command line
 project_file=$(find $project_dir -maxdepth 1 -type f -name "*.prj")
 cmd="$KATALON_KATALON_INSTALL_DIR/katalon -runMode=console -reportFolder=$report_dir -projectPath=$project_file $KATALON_OPTS"
-
 $KATALON_BASE_ROOT_DIR/scripts/xvfb.sh start
 cd $tmp_dir
 eval "$cmd"
-
 cd $current_dir
